e5735e9552a301d3d3e2dae4742f408d57062bfb
a3at28 <a3at28@gmail.com>
2021-02-18 23:21:49 +0300

    IMT.java

--- src/IMT.java
+++ src/IMT.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class IMT {
-}
+public class IMT {
+    public static void main(String[] args) {
+        double mass = 60;
+        double height = 1.69;
+        double IMT = mass / (height * height);
+        System.out.println(IMT);
