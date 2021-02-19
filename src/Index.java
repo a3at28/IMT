f0afc84f9b9a2d23b203e9f8a4672936c401807c
a3at28 <a3at28@gmail.com>
2021-02-19 23:51:21 +0300

    Index.java added

--- src/Index.java
+++ src/Index.java
@@ -1,6 +1,8 @@
-public class IMT {
+public class Index {
     public static void main(String[] args) {
         double mass = 60;
         double height = 1.69;
         double IMT = mass / (height * height);
         System.out.println(IMT);
+    }
+}
